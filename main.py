56b90fd646594056513c4ee6505417e9754eb727
Oryndeles <oryndeles@gmail.com>
2026-07-22 09:01:33 +0200

    Prompt is working and progress.

--- main.py
+++ main.py
@@ -2,30 +2,46 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 from openai.types import Completion, CompletionChoice, CompletionUsage
+import argparse
 
-
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
-if api_key is None:
-    raise RuntimeError("api key is None")
-
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
-
-response = client.chat.completions.create(model="openrouter/free", messages=[
-    {
-        "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    }
-])
-
-print(response.choices[0].message.content)
 
 
 def main():
     print("Hello from ai-agent!")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("api key is None")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+
+    if response.usage is None:
+        raise RuntimeError("Response did not include token usage.")
+    if args.verbose is True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
